fd675528ec80ab4947a72c306243901db8c89d71
Paulo Valadares <paulohva@gmail.com>
2019-06-23 02:08:34 +0100

    Finish tutorial RollABall

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
 
+    public Text countText;
     public float speed;
+    public Text winText;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
 
     private void FixedUpdate()
@@ -27,7 +32,19 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pick Up")) 
         {
-            other.gameObject.SetActive(false);    
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+            //if(count)
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 12)
+        {
+            winText.text  = "You win!";    
         }
     }
 
